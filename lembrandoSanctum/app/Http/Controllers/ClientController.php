e85b9ebb9b838b5014ffc238929c89702040d336
ftreitasyuri <yuriftq@gmail.com>
2025-04-16 17:20:06 -0300

    Upload: Subindo o método delete, agora todas as partes do CRUD estão implementadas

--- lembrandoSanctum/app/Http/Controllers/ClientController.php
+++ lembrandoSanctum/app/Http/Controllers/ClientController.php
@@ -93,6 +93,13 @@ class ClientController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        // delete the client
+        $client = Client::find($id);
+        if($client){
+            $client->delete();
+            return response()->json(['message' => 'Client deleted successfully'], 200);
+        }else{
+            return response()->json(['message' => 'Client not found'], 404);
+        }
     }
 }
