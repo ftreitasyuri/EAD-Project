b4c51482ac420e7986ea8d83dd7c81963e9d9f01
ftreitasyuri <yuriftq@gmail.com>
2025-04-16 16:32:26 -0300

    Upload: Subindo configurações atualizadas, foi criado a model Client, a Factory Clients e a ClientController

---
+++ lembrandoSanctum/database/seeders/ClientSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Client;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Comando para criar 100 clientes baseado na factory
+        Client::factory()->count(100)->create();
+    }
+}
