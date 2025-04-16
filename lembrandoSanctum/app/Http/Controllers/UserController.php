2baf79d6a69d3b9b199f3f173d44fd4d24a704cf
ftreitasyuri <yuriftq@gmail.com>
2025-04-16 18:04:25 -0300

    Upload: Subindo configuração necessária para fazer login e gerar o token de acesso na controller UserController

---
+++ lembrandoSanctum/app/Http/Controllers/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class UserController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+
+        // Validate request
+        $request->validate(
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ],
+            []
+        );
+
+        // Login attempt
+        // $email = $request->email;
+        // $password = $request->password;
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $attempt = auth()->attempt(
+            [
+                'email' => $email,
+                'password' => $password
+            ]
+        );
+
+        if (!$attempt) {
+            return response()->json(
+                [
+                    'error' => 'Unauthorized'
+                ],
+                401
+            );
+        }
+
+        // Authenticate user
+        $user = auth()->user();
+        $token = $user->createToken($user->name)->plainTextToken;
+
+        // Return access token for the api request
+        return response()->json(
+            ['token' => $token]
+        );
+    }
+}
--- lembrandoSanctum/routes/api.php
+++ lembrandoSanctum/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,7 @@ Route::get('/status', function (){
 });
 
 Route::apiResource('clients', ClientController::class);
+
+// Auth routes
+// Route::post('/login', [UserController::class, 'login']);
+Route::post('/login', [UserController::class, 'login']);
